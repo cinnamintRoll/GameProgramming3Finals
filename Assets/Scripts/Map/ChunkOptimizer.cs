162c8f6c2e214400ea5e8f087bb51263819b086a
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2024-03-19 20:11:02 +0800

    Chunk Optimization Added. New Assets Added.

---
+++ Assets/Scripts/Map/ChunkOptimizer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ChunkOptimizer : MonoBehaviour
+{
+    public float maxDist;
+    float dist;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(player.position, transform.position);
+        if (dist > maxDist)
+        {
+
+        }
+    }
+}
