4f3d8f248046f108c14e501e7e33d06dc65ea6ab
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2024-03-19 19:36:33 +0800

    Inifinite Map Generation - Now Active

---
+++ Assets/Scripts/Map/ChunkTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ChunkTrigger : MonoBehaviour
+{
+    MapController mc;
+
+    public GameObject targetMap;
+
+    void Start()
+    {
+        mc = FindObjectOfType<MapController>();
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            mc.currentChunk = targetMap;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (mc.currentChunk == targetMap)
+            {
+                mc.currentChunk = null;
+            }
+        }
+    }
+}
